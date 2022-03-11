fix: quaternion rotation fixed

scripts/box_to_hand.py
@@ -7,35 +7,55 @@ from geometry_msgs.msg import Pose, PoseStamped
 import tf.transformations
 
 
-
 pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=10)
 
 
-# def callback(data):
-#     index = data.name.index("jackal::kinova_arm_right_finger_dist_link")
-#     eef_pose = data.pose[index]
-#     model_pose = eef_pose
-#     model_state = ModelState()
-#     model_state.model_name = "tomato_soup_can_textured"
-#     model_state.pose = model_pose
-#     model_state.reference_frame = "world"
-#     pub.publish(model_state)
 def callback(data, args):
-    initial_eef_pose = args
-    initial_quat = [initial_eef_pose.orientation.x,
+    initial_eef_pose = args[0]
+    initial_object_pose = args[1]
+
+    initial_eef_quat = [initial_eef_pose.orientation.x,
                     initial_eef_pose.orientation.y,
                     initial_eef_pose.orientation.z,
                     initial_eef_pose.orientation.w] 
+    initial_eef_quat_conj = tf.transformations.quaternion_conjugate(initial_eef_quat)
+    initial_object_quat = [initial_object_pose.orientation.x,
+                    initial_object_pose.orientation.y,
+                    initial_object_pose.orientation.z,
+                    initial_object_pose.orientation.w]                
     index = data.name.index("jackal::kinova_arm_end_effector_link")
     current_eef_pose = data.pose[index]
-    current_quat_inv = [current_eef_pose.orientation.x,
+    current_quat = [current_eef_pose.orientation.x,
                     current_eef_pose.orientation.y,
                     current_eef_pose.orientation.z,
-                    -current_eef_pose.orientation.w]
-    rotation_quaternion = tf.transformations.quaternion_multiply(initial_quat, current_quat_inv)
+                    current_eef_pose.orientation.w]
+    rotation_quaternion = tf.transformations.quaternion_multiply(current_quat, initial_eef_quat_conj)
     roation_quaternion_matrix = tf.transformations.quaternion_matrix(rotation_quaternion)
     rotation_matrix = numpy.delete(numpy.delete(roation_quaternion_matrix, 3, 0), 3, 1)
-    print(rotation_matrix)
+    offset = Pose()
+    offset = numpy.array([initial_object_pose.position.x - initial_eef_pose.position.x,
+                          initial_object_pose.position.y - initial_eef_pose.position.y,
+                          initial_object_pose.position.z - initial_eef_pose.position.z])
+
+    new_object_quat = tf.transformations.quaternion_multiply(rotation_quaternion, initial_object_quat)
+    new_offset = rotation_matrix.dot(offset)
+    
+    new_object_pose = Pose()
+    new_object_pose.orientation.x = new_object_quat[0]
+    new_object_pose.orientation.y = new_object_quat[1]
+    new_object_pose.orientation.z = new_object_quat[2]
+    new_object_pose.orientation.w = new_object_quat[3]
+
+    new_object_pose.position.x = current_eef_pose.position.x + new_offset[0]
+    new_object_pose.position.y = current_eef_pose.position.y + new_offset[1]
+    new_object_pose.position.z = current_eef_pose.position.z + new_offset[2]
+
+    new_object_state = ModelState()
+    new_object_state.pose = new_object_pose
+    new_object_state.model_name = "tomato_soup_can_textured"
+    new_object_state.reference_frame = "world"
+    pub.publish(new_object_state)
+
 
 def main():
     rospy.init_node('item_to_gripper', anonymous=True)